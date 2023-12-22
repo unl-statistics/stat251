b0c5a728280b74a1f7ab140638c44480a44a7631
Susan Vanderplas <srvanderplas@gmail.com>
2023-12-22 10:23:58 -0600

    Automatically update categories, etc. and exclude based on week

--- scripts/ignore-future-docs.R
+++ scripts/ignore-future-docs.R
@@ -8,6 +8,16 @@ timezone <- course_settings$`auto-publish`$timezone
 publish_week_before <- course_settings$`auto-publish`$`publish-week-before`
 live_as_of <- course_settings$`auto-publish`$`live-as-of`
 
+
+# Set up weeks to map to dates.
+week_mapping <- course_settings$calendar |>
+  purrr::map_df(as.data.frame) |>
+  dplyr::mutate(date = lubridate::ymd(date, tz = timezone),
+                week_start = date,
+                week_end = date + lubridate::days(7)) |>
+  dplyr::select(-date)
+
+
 publish_cutoff <- if (live_as_of == "Sys.time()") {
   lubridate::with_tz(time = eval(parse(text = live_as_of)),
                      tz = timezone)
@@ -15,25 +25,45 @@ publish_cutoff <- if (live_as_of == "Sys.time()") {
   lubridate::ymd(live_as_of, tz = timezone)
 }
 
+weeks_to_publish <- dplyr::filter(
+  week_mapping,
+  week_start <= publish_cutoff + lubridate::weeks(publish_week_before)
+)
+weeks_to_exclude <- dplyr::anti_join(week_mapping, weeks_to_publish)
+
 # collect data frame of all materials
 materials <- purrr::map(course_settings$schedule, "materials") |>
   purrr::list_flatten() |>
   purrr::map_df(as.data.frame) |>
-  tidyr::nest(categories = categories) |>
+  tidyr::nest(categories = categories)
+
+readings <- dplyr::filter(materials, is.na(href))
+
+materials <- materials |>
+  dplyr::filter(!is.na(href)) |>
   dplyr::arrange(href) |>
+  dplyr::mutate_at(
+    .vars = c("date", "pub.date"),
+    lubridate::ymd, tz=timezone
+  ) |>
+  dplyr::mutate(
+    week_start = lubridate::floor_date(date, "week", week_start = 7)
+  ) |>
+  dplyr::left_join(week_mapping) |>
   dplyr::mutate(
     dir = dirname(href),
     file = basename(href),
-    show_file = stringr::str_remove(file, "^_"),
-    hide_file = paste0("_", show_file),
+    # show_file = stringr::str_remove(file, "^_"),
+    # hide_file = paste0("_", show_file),
     pub.date = ifelse(is.na(pub.date),
-                      lubridate::ymd(date, tz = timezone) - lubridate::weeks(publish_week_before),
-                      lubridate::ymd(pub.date, tz = timezone)),
+                      date - lubridate::weeks(publish_week_before),
+                      pub.date) |> lubridate::as_datetime(tz = timezone),
     is_live = (pub.date <= publish_cutoff) & !is.na(href),
-    is_visible = file.exists(file.path(dir, show_file)),
+    # is_visible = file.exists(file.path(dir, show_file)),
     file_date = lubridate::ymd(date, tz = timezone),
-    current_path = ifelse(is_visible, file.path(dir, show_file), file.path(dir, hide_file)),
-    rename_path = ifelse(is_live, file.path(dir, show_file), file.path(dir, hide_file))
+    # current_path = ifelse(is_visible, file.path(dir, show_file), file.path(dir, hide_file)),
+    # rename_path = ifelse(is_live, file.path(dir, show_file), file.path(dir, hide_file))
+    categories = purrr::map2(categories, name, ~c(as.character(unlist(.x)), .y))
   )
 
 hidden_materials <- dplyr::filter(materials, !is_live) |> tidyr::unnest(categories)
@@ -48,7 +78,7 @@ source("scripts/change-yml.R")
 idx_lines <- readLines("index.qmd")
 yaml_lines <- which(idx_lines == "---")
 index_yml <- yaml::read_yaml(text = idx_lines[min(yaml_lines):max(yaml_lines)])
-index_yml$listing[[1]]$exclude = list(categories = exclude_cats)
+index_yml$listing[[1]]$exclude = list(categories = weeks_to_exclude$name)
 index_yml$input_file <- "index.qmd"
 index_yml$output_file <- "index.qmd"
 
--- scripts/update-dates.R
+++ scripts/update-dates.R
@@ -1,12 +1,23 @@
-# This script populates dates from _schedule.yml to the yml files themselves
+# This script populates dates and weeks from _schedule.yml to the yml files themselves
 
 source("scripts/change-yml.R")
 
 # read in course settings
 course_settings <- yaml::read_yaml("_schedule.yml")
+timezone <- course_settings$`auto-publish`$timezone
 
+# Set up weeks to map to dates.
+week_mapping <- course_settings$calendar |>
+  purrr::map_df(as.data.frame) |>
+  dplyr::mutate(date = lubridate::ymd(date, tz = timezone),
+                week_start = date,
+                week_end = date + lubridate::days(7)) |>
+  dplyr::select(-date)
 
 update_yaml_params <- function(lst) {
+  if(is.null(lst$href)) {
+    return(NULL)
+  }
   if(!file.exists(lst$href)) {
     cli::cli_alert_info("File {lst$href} does not exist.
                          Checking to see if it has been hidden.")
@@ -20,6 +31,19 @@ update_yaml_params <- function(lst) {
   params <- lst[-which(names(lst)%in% c("href", "pub-date"))]
   params$input_file <- lst$href
   params$output_file <- lst$href
+  if ("date" %in% names(lst)) {
+    # Identify which week the assignment falls in
+    assign_week <- dplyr::filter(
+      week_mapping,
+      week_start <= lubridate::ymd(lst$date, tz = timezone),
+      week_end >= lubridate::ymd(lst$date, tz = timezone))
+
+    # Update the category accordingly
+    if (nrow(assign_week) == 1){
+      params$categories <- c(params$categories, assign_week$name)
+    }
+  }
+
   do.call("change_yaml_matter", params)
 }
 
